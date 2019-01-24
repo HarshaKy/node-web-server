16cc136c87c588fb4917c9749b8bbe694be6ff85
HarshaKy <harshaky6573@gmail.com>
2019-01-25 01:06:55 +0530

    handlebars, helper, partials

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello There!</h1>');
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to My App',
-    currentYear: new Date().getFullYear()
+    welcomeMessage: 'Welcome to My App'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
